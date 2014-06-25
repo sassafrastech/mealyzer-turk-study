876c25648fd69856415c52445748949ddc34e75e
Jill Dimond <jill@sassafrastech.com>
2014-06-25 11:31:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MealyzerStudy::Application.config.secret_key_base = 'e56563951ba29885e1d3c1ec949bca519271dfd37bd2ddd66f3415ce2e496228de4bb2b8f9e4a9197f8016392254d833f16f6f88bb140e7239ae836333b9ae8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MealyzerStudy::Application.config.session_store :cookie_store, key: '_mealyzer-study_session'

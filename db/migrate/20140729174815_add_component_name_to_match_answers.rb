fd929be643c2745add9947f2a2ae11ac3a1a3373
Jill Dimond <jill@sassafrastech.com>
2014-08-04 09:42:04 -0400

    fixed add column name

--- db/migrate/20140729174815_add_component_name_to_match_answers.rb
+++ db/migrate/20140729174815_add_component_name_to_match_answers.rb
@@ -1,4 +1,4 @@
-class AddComponentNameToMatchAnswer < ActiveRecord::Migration
+class AddComponentNameToMatchAnswers < ActiveRecord::Migration
   def change
     add_column :match_answers, :component_name, :string
   end

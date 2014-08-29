77f6c04ce0e1858bb21800ed3c3c1ea4ec13b026
Jill Dimond <jill@sassafrastech.com>
2014-08-29 12:57:16 -0400

    testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MealyzerStudy::Application.routes.draw do
 
   root 'home#index'
-  resources :meals, :tags, :matches
+  resources :meals, :tags
   resources :match_answers, controller: 'matches' do
     member do
       get 'completed'

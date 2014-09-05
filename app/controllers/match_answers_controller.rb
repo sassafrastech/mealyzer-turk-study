e0ca9fda7bfaecef52d31865453bc3345676ec56
Jill Dimond <jill@sassafrastech.com>
2014-09-05 11:14:10 -0400

    fixed over counting users

--- app/controllers/match_answers_controller.rb
+++ app/controllers/match_answers_controller.rb
@@ -64,7 +64,7 @@ class MatchAnswersController < ApplicationController
 
   def completed
     @match_answer = MatchAnswer.find(params[:id])
-    reset_session
+    #reset_session
   end
 
   private

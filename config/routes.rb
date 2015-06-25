338f5ea5086cfaba7dbb9549d376c204a8bfc162
Tom Smyth <tom@sassafras.coop>
2015-06-24 21:04:41 -0400

    2476: Tidied routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,17 @@
 MealyzerStudy::Application.routes.draw do
 
   #devise_for :users, :controllers => { :registrations => "registrations" }
-
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  root 'home#index'
-  resources :meals, :tags
-  resources :match_answers
-
-  resources :users
-
-  get 'finish', to: 'users#post_test', as: :post_test
-  put 'finish', to: 'users#update_survey', as: :finish
-  get 'completed', to: 'users#completed', as: :completed
+  resources :mobile_submissions
   post 'subscribe', to: 'push_notifications#subscribe', as: :subscribe
 
-
-  resource :match_answer_group
-
-  resources :mobile_submissions
-
+  # Mechanical Turk routes
+  # resources :tags
+  # resources :users
+  # resources :match_answers
+  # resource :match_answer_group
+  # get 'finish', to: 'users#post_test', as: :post_test
+  # put 'finish', to: 'users#update_survey', as: :finish
+  # get 'completed', to: 'users#completed', as: :completed
 end

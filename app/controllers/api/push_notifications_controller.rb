eaf3ca3866a8954bbafa5c6370b7c9dc2e3ed08a
Tom Smyth <tom@sassafras.coop>
2015-06-26 15:54:34 -0400

    2556: Tidied push controller

--- app/controllers/api/push_notifications_controller.rb
+++ app/controllers/api/push_notifications_controller.rb
@@ -3,16 +3,7 @@ class Api::PushNotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def subscribe
-    # find user
-    Rails.logger.debug("Current user? #{current_user}")
     current_user.update_attributes(token: params[:token])
-
-    # @user = User.where(uid: params[:uid]).last
-    # if @user.nil?
-    #   @user = User.create(uid: params[:uid], token: params[:token])
-    # end
-
-    render :nothing => true
+    render nothing: true
   end
-
 end

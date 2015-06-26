53ba6a590139f1b23e1b834fe030f8ac1aebe47e
Tom Smyth <tom@sassafras.coop>
2015-06-26 15:34:02 -0400

    2476: Added mailer URL options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  before_action :mailer_set_url_options
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,6 +9,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name << :diabetes << :training << :training_stage
     devise_parameter_sanitizer.for(:account_update) << :token
+  end
+
+  # mailer is for some reason too stupid to figure these out on its own
+  def mailer_set_url_options
+    # copy options from the above method, and add a host option b/c mailer is especially stupid
+    default_url_options.merge(:host => request.host_with_port).each_pair do |k,v|
+      ActionMailer::Base.default_url_options[k] = v
+    end
   end
 
   #force_ssl

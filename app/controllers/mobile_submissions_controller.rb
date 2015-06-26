79a4e90252c5e9245f03a08d1dcf5bb322edad1b
Tom Smyth <tom@sassafras.coop>
2015-06-26 15:17:32 -0400

    2476: Fixed a few things in mobile submissions controller

--- app/controllers/mobile_submissions_controller.rb
+++ app/controllers/mobile_submissions_controller.rb
@@ -13,24 +13,12 @@ class MobileSubmissionsController < ApplicationController
     unless Rails.env.development? || Rails.env.test?
       PushNotification.send(@mobile_submission.user.token)
     end
-
-    render nothing: true
   end
 
   private
 
   def mobile_submissions_parameters
-    params.require(:mobile_submission).permit(:protein_user, :fat_user, :carbs_user,
-                                              :fiber_user, :created_at, :photo_file_name,
-                                              :photo_content_type, :photo_file_size, :photo_updated_at,
-                                              :protein_eval, :carbs_eval, :fiber_eval, :fat_eval,
-                                              :evaluated, :meal, :protein_grade,
-                                              :fat_grade, :carbs_grade, :fiber_grade,
-                                              :protein_explain, :fat_explain, :carbs_explain,
-                                              :fiber_explain, :photo_url, :calories_user,
-                                              :calories_grade, :calories_explain, :calories_eval,
-                                              :premeal_bg, :insulin, :reminder,
-                                              :postmeal_bg, :premeal_bg_time, :postmeal_bg_time
-                                              )
+    params.require(:mobile_submission).permit(:protein_eval, :carbs_eval, :fiber_eval, :fat_eval, :calories_eval,
+      :protein_explain, :fat_explain, :carbs_explain, :fiber_explain, :calories_explain)
   end
 end

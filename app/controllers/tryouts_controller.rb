1bb9fbd60a5532c1cb91867af2f6ae385c3cd1dc
Tom Smyth <tom@sassafras.coop>
2015-09-09 09:14:42 -0400

    Disabled caching

--- app/controllers/tryouts_controller.rb
+++ app/controllers/tryouts_controller.rb
@@ -1,6 +1,7 @@
 # Page allowing experimenters to try out the various conditions.
 class TryoutsController < ApplicationController
   def index
+    response.headers['Cache-Control'] = 'no-cache, max-age=0, must-revalidate, no-store'
     reset_session
     session[:tryout_mode] = true
     @fake_turk_params = Hash[*%w(workerId assignmentId hitId).map{ |k| [k, (User.maximum(k) || 0).to_i + 1] }.flatten]

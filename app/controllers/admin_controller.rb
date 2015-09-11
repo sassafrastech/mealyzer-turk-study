ee9d3db85a5e5e23774d539acc406e559b63b514
Tom Smyth <tom@sassafras.coop>
2015-09-11 15:53:22 -0400

    Tweak to admin stats table

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,7 +6,7 @@ class AdminController < ApplicationController
     @fake_turk_params = Hash[*%w(workerId assignmentId hitId).map{ |k| [k, SecureRandom.hex] }.flatten]
     @stats = User.group(:study_id, :study_phase, :condition, :complete).
       order(:study_id, :study_phase, :condition, :complete).count
-    @studies = @stats.keys.map{ |k| k[0] }.uniq
+    @studies = (@stats.keys.map{ |k| k[0] } << Settings.study_id).uniq.sort
   end
 
   def exit

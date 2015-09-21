f193679d9cd1813c152ad427b559296e97655adf
Tom Smyth <tom@sassafras.coop>
2015-09-21 10:35:28 -0400

    Fixed answer tally

--- app/models/answerlet_summarizer.rb
+++ app/models/answerlet_summarizer.rb
@@ -22,27 +22,16 @@ class AnswerletSummarizer
   # Returns top answerlets for all ingredients for current study seed phase
   def top_5_for_all_ingredients
     result = Answerlet.joins(match_answer: :user).
+      select("meal_id, component_name, ingredient, nutrients").
+      select("SUM(CASE WHEN users.study_phase = 'seed' THEN 1 ELSE 0 END) AS ans_count").
+      select("SUM(CASE WHEN users.study_phase = 'explain' THEN 1 ELSE 0 END) AS exp_count").
       where("users.study_id = ?", Settings.study_id).
       where("users.study_phase = 'seed' OR users.study_phase = 'explain'").
       where("users.complete = 't'").
-      group("meal_id", "component_name", "ingredient", "nutrients", "study_phase").
-      order("meal_id, component_name, ingredient, count_all DESC, nutrients, study_phase DESC").count
+      group("meal_id", "component_name", "ingredient", "nutrients").
+      order("meal_id, component_name, ingredient, ans_count DESC, nutrients")
 
-    result = result.map do |data, count|
-      {meal_id: data[0], component_name: data[1], ingredient: data[2],
-        nutrients: JSON.parse(data[3]).map{ |n| Meal::GRP_ABBRVS[n] }.join,
-        study_phase: data[4], count: count}
-    end
-
-    result = result.group_by{ |h| h.slice(:meal_id, :component_name, :ingredient) }.each do |mci, data|
-      data.group_by{ |h| h[:nutrients] }.each do |nutrients, data2|
-        data2[0][:ans_count] = data2[0].delete(:count)
-        data2[0].delete(:study_phase)
-        if data2[1]
-          data2[0][:exp_count] = data2[1][:count]
-          data.delete(data2[1])
-        end
-      end
-    end
+    result.map{ |r| r.attributes.with_indifferent_access }.
+      group_by{ |h| h.slice(:meal_id, :component_name, :ingredient) }
   end
 end
